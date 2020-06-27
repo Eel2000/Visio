3538719b23666f48e65245b290fb51a6c85c90ef
eliezerbwana@gmaill.com <eliezerbwana@gmaill.com>
2020-06-27 18:52:39 +0200

    ajusting front end design

--- Visio_Beta/Startup.cs
+++ Visio_Beta/Startup.cs
@@ -33,6 +33,8 @@ namespace Visio_Beta
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
