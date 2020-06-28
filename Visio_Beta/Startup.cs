7f067b43847a80a897a30ff3586ae0799186abd7
eliezerbwana@gmaill.com <eliezerbwana@gmaill.com>
2020-06-28 03:16:44 +0200

    add pages finish

--- Visio_Beta/Startup.cs
+++ Visio_Beta/Startup.cs
@@ -30,11 +30,9 @@ namespace Visio_Beta
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
-
-            
+            services.AddRazorPages();          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
