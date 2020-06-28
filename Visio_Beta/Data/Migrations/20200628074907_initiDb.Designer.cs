fba544a02d665f87d94d4f53dda12727b1aa899c
eliezerbwana@gmaill.com <eliezerbwana@gmaill.com>
2020-06-28 09:52:41 +0200

    first migration to initialize database

--- Visio_Beta/Data/Migrations/20200628074907_initiDb.Designer.cs
+++ Visio_Beta/Data/Migrations/20200628074907_initiDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Visio_Beta.Data;
 
 namespace Visio_Beta.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200628074907_initiDb")]
+    partial class initiDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
